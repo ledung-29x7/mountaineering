08054f5cdf810140a81ce9836121f7ae04524d91
tienhnpl <dotien.21797@gmail.com>
2023-12-16 01:07:45 +0700

    delete

--- src/body.js
+++
@@ -1,9 +0,0 @@
-import './body.css';
-import { BrowserRouter ,Route,Router,Routes} from "react-router-dom";
-const Body = ()=>{
-    return(
-        <body>
-        </body>
-    )
-}
-export default Body ;
--- src/page/media/photos/photos.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-
-const Photos =()=>{
-    return(
-        <h1>photos</h1>
-    );
-}
-export default Photos;
