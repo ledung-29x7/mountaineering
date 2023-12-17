440052bd58baf970c79536813f500f4784508f13
tienhnpl <dotien.21797@gmail.com>
2023-12-17 18:50:41 +0700

    update

--- src/page/Clubs/detail/AmericanAlpineClub.js
+++ src/page/Clubs/detail/AmericanAlpineClub.js
@@ -3,20 +3,23 @@ import img2 from "../../../img/clubs/American Alpine Club/2.png";
 import img3 from "../../../img/clubs/American Alpine Club/3.jpg";
 import img4 from "../../../img/clubs/American Alpine Club/4.jpg";
 import img5 from "../../../img/clubs/American Alpine Club/5.jpg";
-import '../../css/dt.css'
+// import '../../css/dt.css';
+import '../../css/club_css.css';
 function AAC(){
     return (
         <div className="test">
             <h1>American Alpine Club</h1>
-            <img src={img1}></img>
+            <div className="logoClubs"><img className="logoACC" src={img1}></img>
+            </div>
             <li><strong>MISSION:</strong> To share and support our passion for climbing and respect for the places we climb.</li>
             <li><strong>VISION:</strong> A united community of competent climbers and healthy climbing landscapes. </li>
             <li><strong>AUDIENCE:</strong> Everyone who loves climbing.</li>
             <li><strong>CORE VALUES:</strong> Authoritative climbing information, knowledge and resources benefit and inspire us and future generations. Advocacy and leadership advance our climbing interests and promote conservation. Community and competency strengthen and embolden us to push our limits.</li>
             <h3>1. A brief history of the Club</h3>
             <p>Read Honorary President William Lowell Putnam's Speech on Club History: "A Great Many Years"</p>
-            <img src={img2}></img>
+            <div className="float"><img src={img2}></img><br></br>
             <span>AAC founding member Fanny Bullock Workman.</span>
+            </div>
             <p>Established in 1902 by the nation's leading climbers and conservationists, The American Alpine Club promotes and preserves the climbing way of life. The Mission Statement reads: "We provide knowledge and inspiration, conservation and advocacy, and logistical support for the climbing community." The AAC evolved from a social club for elite adventurers located on the East Coast to a more centrally located Denver-area organization that actively participates in international dialogue about environmental policy, high altitude safety and medicine, innovation in alpine tools, clothing and survival technologies, sponsors expeditions, and investigates controversies in the world of exploration.</p>
             <p>John Muir (1838-1914), considered by some to be a founder of the wilderness preservation movement, was the American Alpine Club's second president (1908-1910). Four women, including Annie Peck (1850-1935) and Fanny Bullock Workman (1859-1925), were among the founding members, all of them trouser-wearing suffragettes and strong leaders. Peck was the first to summit Peru's 22,205' Mt. Huascaran in 1908. Workman, along with her husband William, was a Himalayan cartographer and explorer, and competed quite publicly with Peck for the women's altitude record.</p>
             <p>The AAC was associated through its officials and members with many significant American and International organizations, including the Appalachian Mountain Club, Sierra Club, Explorer's Club, Canadian Alpine Club and Alpine Club of Britain, and it was integral to the founding of the Union International des Associations d'Alpinism (UIAA) in 1930 and the Arctic Institute of North America in 1948. The AAC was central in the formation, in 1940, of the 10th Mountain Division of the U.S. Army, an elite division specializing in warfare in harsh alpine conditions.</p>
@@ -25,14 +28,20 @@ function AAC(){
             <h3>2. Awards</h3>
             <p>The American Alpine Club proudly presents yearly awards to those with outstanding achievements in conservation, climbing and mountaineering, mountain literature, and dedication to the Club.</p>
             <li><strong>Honorary Members</strong>- Honorary Membership is the highest award the AAC has to offer. It is given to those individuals who have had a lasting and highly significant impact on the advancement of the climbing craft. The AAC is proud to recognize the following individuals.</li>
-            <img src={img3}></img>
+            <div className="block"><img src={img3}></img>
             <span>AAC Honorary Member Fred Beckey with Former President Mark Kroese</span>
+            </div>
             <li><strong>The Robert Hicks Bates Award</strong>- Robert Hicks Bates, a renowned explorer, alpinist, Peace Corps director and educator, accomplished numerous first ascents in Alaska and was a member of the American expeditions to K2 in 1938 and 1953. His literary contributions describing the K2 expeditions and his life of adventure in the mountains inspired young American climbers. He served the American Alpine Club in many capacities: member of the editorial board of the American Alpine Journal, Board member, President and Honorary President. Above all, he devoted his life to promoting the education and development of American youth through his years as a teacher at Phillips Exeter Academy, service with the Peace Corps, and other endeavors. To recognize his many contributions to American mountaineering and education of our nation's youth, the American Alpine Club established the "Robert Hicks Bates Award" for outstanding accomplishment by a young climber. It is to recognize a young climber who in the judgment of the selection committee has demonstrated exceptional skill and character in the climbing or mountaineering arts and has outstanding promise for future accomplishment.</li>
-            <img src={img4}></img>
+            <div className="block"><img src={img4}></img>
             <span>2017 Bates Awardee Mason Earle.</span>
+            </div>
+            
             <li><strong>The David Brower Conservation Award</strong>- David R. Brower, an active alpinist and member of the famed 10th Mountain Division with over seventy first ascents across the country, was a pioneer in the environmental movement in this country and abroad. He was twice nominated for the Nobel Peace Prize for his work in conservation. Brower was the first Executive Director of the Sierra Club, founder of Friends of the Earth, and serves as Chairman of Earth Island Institute. His commitment to conservation contributed substantially to the establishment of sound global environmental practices. Brower was a lifetime member of the AAC and served as the organization's Vice President from 1956-58. The David R.Brower Award, created in 1991, is an annual award recognizing leadership and commitment to preserving mountain regions worldwide. The awardee, whose active personal role deserves public recognition, has made an important difference as a pathfinder, innovator and on the ground contributor who has motivated others to take action.</li>
+            <div className="block">
             <img src={img5}></img>
             <span>Angelo Heilprin Citation Award Winners</span>
+            </div>
+            
         </div>
     );
 }
--- src/page/Clubs/detail/TheAlpineClub.js
+++ src/page/Clubs/detail/TheAlpineClub.js
@@ -1,16 +1,18 @@
-import img1 from "../../../img/clubs/The Alpine Club/1.jpg";
+import img1 from "../../../img/clubs/The Alpine Club/1.PNG";
 import img2 from "../../../img/clubs/The Alpine Club/2.jpg";
 import img3 from "../../../img/clubs/The Alpine Club/3.webp";
+import '../../css/club_css.css';
 function TheAlpineClub(){
     return (
         <div className="test">
             <h1>The Alpine Club</h1>
-            <img className="img_bv" src={img1} alt=""></img>
+            <div className="logoACC"><img className="logo" src={img1} alt=""></img></div>
             <h3>1. A brief history of the Club</h3>
             <h5>1.1. History</h5>
             <p>The Alpine Club was founded on 22 December 1857 by a group of British mountaineers at Ashley's Hotel in London. The original founders were active mountaineers in the Alps and instrumental in the development of alpine mountaineering during the Golden Age of Alpinism (1854–1865). E. S. Kennedy was the first chairman of the Alpine Club but the naturalist, John Ball, was the first president. Kennedy, also the first vice-president, succeeded him as president of the club from 1860 to 1863. In 1863, the club moved its headquarters to the Metropole Hotel.</p>
-            <img src={img2}></img>
+            <div className="float"><img src={img2}></img><br></br>
             <span>Title page from Peaks, Passes, and Glaciers, 1859, edited by John Ball, first president of the Alpine Club</span>
+            </div>
             <p>The Alpine Club is specifically known for having developed early mountaineering-specific gear including a new type of rope. The goal was to engineer a strong and light rope that could be carried easily. A committee of the club tested samples from suppliers and prepared a specification in the early 1900s. The official Alpine Club Rope was then made by John Buckingham of Bloomsbury. It was made from three strands of manila hemp, treated to be rot proof and marked with a red thread of worsted yarn.[5] From the 19th century British mountaineers and members of the Alpine Club were instrumental in the popularisation of mountainteering in Norway among the international mountaineering community, with William Cecil Slingsby's influential book, Norway, the Northern Playground. The Alpine Club was the role model of the Norwegian Alpine Club, the third oldest of its kind worldwide.</p>
             <p>The present Alpine Club members remain extremely active in the Alps and the Greater Ranges, as well as in mountain arts, literature and science.</p>
             <p>For many years it had the characteristics of a London-based Gentlemen's club, including a certain imprecision in the qualification for membership (said to have been 'A reasonable number of respectable peaks'). Until 1974, the club was strictly for men only, but in 1975, within months of membership being opened to women, a merger with the Ladies' Alpine Club was agreed, and the Club thus gained about 150 new members.[6] By the last quarter of the 20th century, the club had evolved into Britain's senior mountaineering club, with a clear qualification for membership, for both men and women, and an 'aspirant' grade for those working towards full membership. However, it still requires prospective members to be proposed and seconded by existing members.</p>
