d94283823b199e6f5886a218ade53d3eb1e651ac
dung <dung@192.168.2.4>
2023-12-17 15:42:12 +0700

    dung

--- src/page/Clubs/detail/TheHimalayanClub.js
+++ src/page/Clubs/detail/TheHimalayanClub.js
@@ -1,3 +1,5 @@
+import React from "react";
+import Maps from "../../../components/maps";
 import img1 from "../../../img/clubs/The Himalayan Club/THC1.jpg";
 import img2 from "../../../img/clubs/The Himalayan Club/THC2.jpg";
 import img3 from "../../../img/clubs/The Himalayan Club/THC3.jpg";
@@ -17,6 +19,7 @@ function TheHimalayanClub(){
             <p>The club publishes an annual journal called The Himalayan Journal which is the most authentic record of Himalayan mountaineering activity since 1929. All members of the club receive the journal and are encouraged to contribute to it in different ways.</p>
             <img className="img_bv" src={img5} alt=""></img>
             <p>The club has set up annual awards in the field of mountaineering and mountain literature in memory of certain prominent members. The awards include the Kekoo Naoroji Book Award for the best book on the Himalaya, and the Jagdish Nanavati Award for Excellence in Mountaineering for an Indian expedition that follows criteria laid out and submits a report that matches the defined guidelines.</p>
+            <Maps name='The Himalayan Club' ggmap="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2779.939608471216!2d72.8248173859068!3d18.987326096644757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7ce61cf2d05e5%3A0x629e22d379588135!2sTurf%20Estate%2C%20315%2C%20Shakti%20Mills%20Ln%2C%20Mahalakshmi%2C%20Mumbai%2C%20Maharashtra%20400011%2C%20%E1%BA%A4n%20%C4%90%E1%BB%99!5e0!3m2!1svi!2sus!4v1702802189941!5m2!1svi!2sus"></Maps>
         </div>
     );
 }
--- src/page/Clubs/detail/TheNewZealandAlpineClub.js
+++ src/page/Clubs/detail/TheNewZealandAlpineClub.js
@@ -1,3 +1,5 @@
+import React from "react";
+import Maps from "../../../components/maps";
 import img1 from "../../../img/clubs/The New Zealand Alpine Club/nz1.png";
 import img2 from "../../../img/clubs/The New Zealand Alpine Club/nz2.jpg";
 function TheNewZealandAlpineClub(){
@@ -7,6 +9,7 @@ function TheNewZealandAlpineClub(){
             <img className="img_bv" src={img1} alt=""></img>
             <p>The New Zealand Alpine Club (NZAC) was founded in 1891 and is one of the oldest alpine clubs in the world. The NZAC is the national climbing organization in New Zealand and is a member of the Union Internationale des Associations d’Alpinisme. It has over 4000 members who are spread across twelve sections, eleven in New Zealand and one in Australia, plus members in other countries</p>
             <img className="img_bv" src={img2} alt=""></img>
+            <Maps name='The New Zealand Alpine Club' ggmap="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2891.8162158154323!2d172.6534622762976!3d-43.54787278297816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d3189fbb2d4b501%3A0x9e734f7106a181ec!2sNew%20Zealand%20Alpine%20Club%20Inc.!5e0!3m2!1svi!2sus!4v1702802394648!5m2!1svi!2sus"></Maps>
         </div>
     );
 }
